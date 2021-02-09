094e513fbb5d4044edcfbe374c080149efbff824
Dmitry Mikhaylov <dmik@vt.edu>
2021-02-09 12:13:38 -0500

    BFS in routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,5 @@
 import csv
 import json
-
 import helper
 
 def read_airlines(filename='airlines.dat'):
@@ -12,7 +11,7 @@ def read_airlines(filename='airlines.dat'):
     return airlines
 
 def read_airports(filename='airports.dat'):
-    airports = {}
+    airports = {} # Build a map from code to name
     with open(filename) as f:
         reader = csv.reader(f)
         for line in reader:
@@ -20,21 +19,33 @@ def read_airports(filename='airports.dat'):
     return airports
 
 def read_routes(filename='routes.dat'):
-    routes = {}
+    routes = {} # Build a map from source to dest
     with open(filename) as f:
-        reader = scv.reader(f)
+        reader = csv.reader(f)
         for line in reader:
-            source, desc = line[2], line[4]
+            source, dest = line[2], line[4]
             if source not in routes:
                 routes[source] = []
             routes[source].append(dest)
     return routes
-    return {}
-
 
 def find_paths(routes, source, dest, max_segments):
-    # Run a graph search algorithm to find paths from source to dest.
-    return {}
+    # Run the BFS search
+    frontier = {source}
+    seen = {source: {(source, )}}
+    for steps in range(max_segments):
+        next_frontier = set()
+        for airport in frontier:
+            for target in routes.get(airport, ()):
+                if target not in seen:
+                    next_frontier.add(target)
+                    seen[target] = set()
+                for path in seen[airport]:
+                    if len(path) != steps + 1:
+                        continue
+                    seen[target].add(path + (target, ))
+        frontier = next_frontier
+    return seen[dest]
 
 def rename_path(path, airports):
     return tuple(map(airports.get, path))
@@ -44,11 +55,18 @@ def main(source, dest, max_segments):
     airlines = read_airlines()
     airports = read_airports()
     routes = read_routes()
-
-    paths = find_paths(routes, source, dest, max_segments)
-    output = {}  # Build a collection of output paths!
     
-    # Don't forget to write the output to JSON!
+    paths = find_paths(routes, source, dest, max_segments)
+    output = {}  # Again, could use a collections.defaultdict(list) here.
+    for path in paths:
+        segments = len(path) - 1
+        if segments not in output:
+            output[segments] = []
+        output[segments].append(rename_path(path, airports))
+    
+    with open(f"{source}->{dest} (max {max_segments}).json", 'w') as f:
+        json.dump(output, f, indent=2, sort_keys=True)
+
 
 if __name__ == '__main__':
     parser = helper.build_parser()
